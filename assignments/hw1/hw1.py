736629c9836cf225874d5c2ad60058c6c8d8a927
ItsTheGreenG <97689847+ItsTheGreenG@users.noreply.github.com>
2022-01-19 21:03:53 -0500

    Stuff and things

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -15,23 +15,34 @@ def calc_rec_area():
     length = eval(input("Enter the length: "))
     width = eval(input("Enter the width: "))
     area = length * width
-    print("Area =", area)
+    print("Area = ", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the Length:"))
+    width = eval(input("Enter the width:"))
+    height = eval(input("Enter the Height:"))
+    volume = length * width * height
+    print("volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_Shots = eval(input("Enter the Total Shots:"))
+    shots_Made = eval(input("Enter the Shots Made:"))
+    shot_Percentage = "{:.0%}".format(shots_Made / total_Shots)
+    print("Shot Percentage =", shot_Percentage)
 
 
 def coffee():
-    pass
+    pound_Total = eval(input("Enter how many pounds you would like:"))
+    final_cost = (10.50 * pound_Total + .86 * pound_Total + 1.50)
+    print("Your total is: =", final_cost)
 
 
 def kilometers_to_miles():
-    pass
+    kilos = eval(input("How many Kilos did you travel?:"))
+    miles = (kilos / 1.61)
+    print("You traveled this many miles:", miles)
 
 
 if __name__ == '__main__':
