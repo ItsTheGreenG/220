7f272c344c74e55c20ee2d880f53bc3d40da96e8
ItsTheGreenG <schlagenhafton@g.cofc.edu>
2022-02-22 20:59:47 -0500

    lab 6

--- labs/lab6/lab6.py
+++ labs/lab6/lab6.py
@@ -1,36 +1,41 @@
 from graphics import *
+import math
 
 
 
 
 
-
-"""def vigenere_graphic():
+def vigenere_graphic():
   win = GraphWin("Vigenere Cipher", 500, 500)
   Text(Point(75, 150), "Message to code").draw(win)
   Text(Point(75, 230), "Enter Keyword ").draw(win)
   Text(Point(250,350), "Resulting Message").draw(win)
-  inputText = Entry(Point(280,150), 30)
+  inputText = Entry(Point(280, 150), 30)
   inputText.setText("")
   inputText.draw(win)
-  inputText2 = Entry(Point(280,230), 20)
+  inputText2 = Entry(Point(280, 230), 20)
   inputText2.setText("")
   inputText2.draw(win)
   outputText = Text(Point(150, 150), " ")
   outputText.draw(win)
   button = Text(Point(220, 300), "Convert It ")
-  button.draw(win)
-  Rectangle(Point(180,250),Point(280,320)).draw(win)
+  Rectangle(Point(180,250), Point(280,320)).draw(win)
   win.getMouse()
   button.undraw()
-  #Rectangle.undraw()
+  # Attempted Theory/Code for the encryption
 
   #Converting Input
-
-  message = float(inputText.setText())
+  message = inputText.getText()
+  keyword = inputText2.getText()
+  keyword.upper()
+  keyword.replace(" ", "")
+  keywordlist = []
+  message.upper()
+  message.replace(" ", "")
   print(message)
-
-
+  for message in keywordlist:
+    Encription = (message + keyword) % 26
+    print(Encription)
 
 
   win.getMouse()
@@ -41,13 +46,8 @@ from graphics import *
 
 
 
-vigenere_graphic()"""
+vigenere_graphic()
 
-def cipher():
-    message = input("Type Message Here")
-    keyword = input("Insert Keyword Here")
-    message.split(" ")
-    keyword.split(" ")
-    cipher1 = len(message) + len(keyword)
-    print(cipher1)
-cipher()
+#def cipher():
+
+#cipher()
