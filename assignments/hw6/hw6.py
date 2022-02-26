6dd62338a27189df0f50a4cf0ced39a7df236a7f
ItsTheGreenG <97689847+ItsTheGreenG@users.noreply.github.com>
2022-02-25 19:55:32 -0500

    hw6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,54 +1,81 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Owen Schlagenhaft
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: multiple cipher and string problems
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
-
+import math
 
 def cash_converter():
-    pass
+    dollar = input("Enter an Integer:")
+    dollar_format = int(dollar)
+    dollar_decimal = "{:.2f}".format(dollar_format)
+    print("That is $", dollar_decimal)
+
 
 
 def encode():
-    pass
+    message = input("Enter a message:")
+    key = input("Enter a key:")
+    encryption = ""
+    for i in range(len(message)):
+        letters = ord(message[i]) - 65
+        shift = ord(key[i % len(key)])-65
+        applied_shift = shift + letters
+        code = chr(applied_shift)
+        encryption = encryption + applied_shift
+    print(encryption)
 
 
 def sphere_area(radius):
-    pass
+    surface_area = 4.0 * math.pi * radius**2.0
+    return surface_area
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4.0/3.0) * math.pi * radius**3.0
+    return volume
 
 
 def sum_n(number):
-    pass
+    sum = (number*(number +1))/2
+    return sum
 
 
 def sum_n_cubes(number):
-    pass
+    sum = (number**2*(number+1)**2)/4
+    return sum
 
 
 def encode_better():
-    pass
+    message = input("Enter a message:")
+    key = input("Enter a key:")
+    encryption = ""
+    for i in range(len(message)):
+        letters = ord(message[i]) - 65
+        shift = ord(key[i % len(key)])-65
+        new_code = letters + shift
+        rerun = new_code % 58
+        character = rerun + 65
+        new = chr(character)
+        encryption = encryption + new
+    print(encryption)
 
 
 if __name__ == '__main__':
-    # cash_converter()
-    # encode()
-    # res = sphere_area(13)
-    # print(res)
-    # res = sphere_volume(13)
-    # print(res)
-    # res = sum_n(100)
-    # print(res)
-    # res = sum_n_cubes(13)
-    # print(res)
-    # encode_better()
-    pass
+     cash_converter()
+     encode()
+     res = sphere_area(13)
+     print(res)
+     res = sphere_volume(13)
+     print(res)
+     res = sum_n(100)
+     print(res)
+     res = sum_n_cubes(13)
+     print(res)
+     encode_better()
+pass
--- notes/practice.py
+++ notes/practice.py
@@ -11,6 +11,31 @@ import math
 #my_circle = Circle(middle, 40)
 
 #my_circle.draw(win)
-#input('Hit enter to close')
+#input('Hit enter to close'
+
+#def sum_diff(x,y):
+    #sum_value = x + y
+    #diff_value = x-y
+    #return sum_value(), diff_value()
 
 
+#my_sum, my_diff = sum_diff(10,7)
+#print(my_sum,my_diff)
+#sum_diff()
+
+
+'''def get_discount(price, sale):
+   price = price * (1-sale)
+   return price
+
+price = 100
+price = get_discount(price, .20)
+print(price)'''
+
+def change_point(p, x, y):
+   p.move(x,y)
+
+p1 = Point(2,3)
+print(p1)
+change_point(p1, 100, 200)
+print(p1)
