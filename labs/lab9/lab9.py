f7cc0907da4d63585dd12fe886b3f76d83954f96
ItsTheGreenG <97689847+ItsTheGreenG@users.noreply.github.com>
2022-03-22 17:57:39 -0400

    Lab9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -1,11 +1,17 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Owen Schlagenhaft
+Lab9.py
+Created a game of Tic Tack Toe!
 """
-
+import math
 
 def build_board():
+    num_list = [1,2,3,4,5,6,7,8,9]
+    print_board(num_list)
+    return num_list
     pass
+
+
 
 
 def print_board(board):
@@ -37,18 +43,98 @@ def print_board(board):
 
 
 def is_legal(board, position):
-    pass
+    if position > 9:
+        print("Not Legal too high")
+        return False
+    if position < 1:
+        print("Not Legal too low")
+        return False
+    else:
+        return True
+
 
 
 def fill_spot(board, position, character):
+    if is_legal(board,position):
+        board[position-1] = character
+    print_board(board)
     pass
-
 
 def winning_game(board):
-    pass
+    # Horizantal wins for x conditions
+    if board[0] == "x" and board[1] == "x" and board[2] == "x":
+        print("game over X wins!")
+        return True
+    if board[3] == "x" and board[4] == "x" and board[5] == "x":
+        print("game over X wins!")
+        return True
+    if board[6] == "x" and board[7] == "x" and board[8] == "x":
+        print("game over X wins!")
+        return True
+    # Vertical win condition for x
+    if board[0] == "x" and board[3] == "x" and board[6] == "x":
+        print("game over X wins!")
+        return True
+    if board[1] == "x" and board[4] == "x" and board[7] == "x":
+        print("game over X wins!")
+        return True
+    if board[2] == "x" and board[5] == "x" and board[8] == "x":
+        print("game over X wins!")
+        return True
+    # diagonal win condtion for x
+    if board[0] == "x" and board[4] == "x" and board[8] == "x":
+        print("game over X wins!")
+        return True
+    if board[2] == "x" and board[4] == "x" and board[6] == "x":
+        print("game over X wins!")
+        return True
+    # Horizantal wins for o conditions
+    if board[0] == "o" and board[1] == "o" and board[2] == 'o':
+        print("game over o wins!")
+        return True
+    if board[3] == "o" and board[4] == "o" and board[5] == 'o':
+        print("game over o wins!")
+        return True
+    if board[6] == "o" and board[7] == "o" and board[8] == 'o':
+        print("game over o wins")
+        return True
+    # Vertical win condition for o
+    if board[0] == "o" and board[3] == "o" and board[6] == 'o':
+        print("game over o wins!")
+        return True
+    if board[1] == "o" and board[4] == "o" and board[7] == 'o':
+        print("game over o wins!")
+        return True
+    if board[2] == "o" and board[5] == "o" and board[8] == 'o':
+        print("game over o wins!")
+        return True
+    # diagonal win condtion for o
+    if board[0] == "o" and board[4] == "o" and board[8] == 'o':
+        print("game over o wins!")
+        return True
+    if board[2] == "o" and board[4] == "o" and board[6] == 'o':
+        print("game over o wins!")
+        return True
+    else:
+        return False
 
 
 def game_over(board):
+    boardstr = ''.join([str(elem) for elem in board])
+    count = 0
+    for i in boardstr:
+        if i.isdigit():
+            count += 1
+
+    if count == 0:
+        print("Cats Game")
+        return True
+    if count > 0:
+        return False
+    if winning_game(board) == True:
+        print("Game Over")
+        return True
+
     pass
 
 
@@ -57,12 +143,27 @@ def get_winner(board):
 
 
 def play(board):
+    while not game_over(board):
+        print("player 1 enter a position to put an x\n")
+        play = eval(input(":?"))
+        fill_spot(board, play, "x")
+        winning_game(game)
+        if not game_over(board):
+            print("player 2 enter a position to put an o\n")
+            play2 = eval(input(":?"))
+            fill_spot(board, play2, "o")
+            winning_game(game)
+
     pass
 
 
 def main():
+
+
     pass
 
 
 if __name__ == '__main__':
     main()
+    game = build_board()
+    play(game)
